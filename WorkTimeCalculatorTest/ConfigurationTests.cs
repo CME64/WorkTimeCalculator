fd8c8df428440a2e8b34810d6c14cde99e273197
Mohammed I. Alsayed <al3ashiq64@gmail.com>
2021-08-26 10:09:11 +0300

    new tests

--- WorkTimeCalculatorTest/ConfigurationTests.cs
+++ WorkTimeCalculatorTest/ConfigurationTests.cs
@@ -64,5 +64,34 @@ namespace WorkTimeCalculatorTest {
             var ex = Assert.Throws<ArgumentException>(() => new WorkTimeCalculator(DayShifts, Holidays));
             Assert.Equal($"Holiday end preceeds the start [from {Holidays[0].Start} to {Holidays[0].End}]", ex.Message);
         }
+
+        [Fact]
+        public void ExactPeriodRange() {
+            Dictionary<DayOfWeek, List<WorkShift>> DayShifts = new Dictionary<DayOfWeek, List<WorkShift>>() {
+                { DayOfWeek.Sunday, new List<WorkShift>(){
+                    new WorkShift(){ Start = new TimeSpan(0,0,0), End = new TimeSpan(24,0,0)}
+                } }
+            };
+
+            List<HolidayConfig> Holidays = new List<HolidayConfig>();
+            var c = new WorkTimeCalculator(DayShifts, Holidays);
+
+            Assert.NotNull(c);
+        }
+
+        [Fact]
+        public void FullDaysWork() {
+            Dictionary<DayOfWeek, List<WorkShift>> DayShifts = new Dictionary<DayOfWeek, List<WorkShift>>() {
+                { DayOfWeek.Sunday, new List<WorkShift>(){
+                    new WorkShift(){ Start = new TimeSpan(0,0,0), End = new TimeSpan(24,0,0)}
+                } }
+            };
+
+            List<HolidayConfig> Holidays = new List<HolidayConfig>();
+
+            var c = new WorkTimeCalculator(DayShifts, Holidays);
+            Assert.Equal(new TimeSpan(5,0,0), c.CalculateWorkTime(new DateTime(2021,8,1,10,0,0), new DateTime(2021, 8, 1, 15, 0, 0)));
+        }
     }
+    
 }
